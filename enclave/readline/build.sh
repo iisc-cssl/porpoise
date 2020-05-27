b938f172574631c5b861a1187bc4fa45f281fb26
Kripa Shanker <iamkripashanker@gmail.com>
2020-05-27 14:45:18 +0530

    fix10

--- enclave/readline/build.sh
+++ enclave/readline/build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+version=8.0
+url=https://ftp.gnu.org/gnu/readline/readline-8.0.tar.gz
+tarname=readline-$version.tar.gz
+if [ ! -f $tarname ]
+then
+	wget $url
+fi
+tar -xvf $tarname
+
+cd readline-$version
 rm -rf build
 mkdir build
 cd build
@@ -6,5 +16,5 @@ cd build
 ../configure CC=gcc CFLAGS="-fPIC -D_FORTIFY_SOURCE=0"
 make
 echo $PWD
-cp libreadline.a ../../lib
-cp libhistory.a ../../lib
+cp libreadline.a ../../../lib
+cp libhistory.a ../../../lib
