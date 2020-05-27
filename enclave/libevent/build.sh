c26c6060fd774702e1b07bd07b575765dc8aea12
Kripa Shanker <iamkripashanker@gmail.com>
2020-05-27 14:36:44 +0530

    fix9

--- enclave/libevent/build.sh
+++ enclave/libevent/build.sh
@@ -2,15 +2,14 @@ if [ ! -f libevent-2.1.11-stable.tar.gz ]
 then
 	wget https://github.com/libevent/libevent/releases/download/release-2.1.11-stable/libevent-2.1.11-stable.tar.gz
 fi
-tar -xvf libevent-2.1.11-stable.tar.gz --strip 1
-
+tar -xvf libevent-2.1.11-stable.tar.gz
+cd libevent-2.1.11-stable
 sh autogen.sh
 rm -rf build
 mkdir build
 cd build
-../configure --prefix=$PWD/../install CFLAGS="-fPIC -D_FORTIFY_SOURCE=0"
+../configure --prefix=$PWD/../../install CFLAGS="-fPIC -D_FORTIFY_SOURCE=0"
 make
 make install-am
-cd ..
-cp install/lib/*.a ../lib
+cp ../../install/lib/*.a ../../../lib
 
