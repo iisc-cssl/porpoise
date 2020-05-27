e255b0d427ec2abf0ccb71dee69c99a715f89a97
Kripa Shanker <iamkripashanker@gmail.com>
2020-05-27 15:02:06 +0530

    fix11

--- enclave/ncurses/build.sh
+++ enclave/ncurses/build.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+version=6.1
+tarname=ncurses-$version.tar.gz
+url=https://ftp.gnu.org/pub/gnu/ncurses/ncurses-$version.tar.gz
+
+if [ ! -f $tarname ]
+then
+	wget $url
+fi
+tar -xvf $tarname
+cd ncurses-$version
+
 rm -rf build;
 mkdir build;
 cd build
-../configure CFLAGS="-D_FORTIFY_SOURCE=0 -O0 -g -fPIC" --prefix=$PWD/_inst
+../configure CFLAGS="-D_FORTIFY_SOURCE=0 -fPIC" --prefix=$PWD/../install
 make
-cp lib/*.a ../../lib
+cp lib/*.a ../../../lib
